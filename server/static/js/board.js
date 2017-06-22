afc822d1b7a32e99b86c97e3b7628293c1d6e570
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-06-22 11:51:14 -0400

    Add Fullscreen Modal

--- server/static/js/board.js
+++ server/static/js/board.js
@@ -4,6 +4,7 @@ function addBoardElement(newBob) {
     .append(createBoardElement(newBob));
   $("bobbles").append($bob);
   adjustBobSize();
+  initModals();
 }
 
 function popluateBoard(bobbles) {
@@ -15,6 +16,7 @@ function popluateBoard(bobbles) {
     $("bobbles").append($bob);
   }
   adjustBobSize();
+  initModals();
 }
 
 function adjustBobSize() {
@@ -43,6 +45,27 @@ function adjustBobSize() {
   });
   console.log("bob size adjusted");
 }
+function initModals(){
+  let $bobs= $("bob");
+  $.each($bobs, function(index, bob) {
+    $modalLink =$("#link"+bob.id);
+    $($modalLink).animatedModal({
+      animatedIn:"zoomIn",
+      animatedOut:"zoomOut",
+      color:"#fbf3f2",
+      modalTarget: "modal" + bob.id,
+      beforeOpen: function() {
+      },
+      afterOpen: function() {
+      },
+      beforeClose: function() {
+      },
+      afterClose: function() {
+      }
+    })
+  });
+}
+
 
 function createBoardElement(bob) {
 
@@ -59,12 +82,6 @@ function createBoardElement(bob) {
     }
     return $chips;
   }
-  let $bobNav =
-  tagChips(bob)
-  .append($('<div></div>').addClass("card-action card-action--thin right-align")
-    .append(buttonIcon("thumb_up"))
-    .append(buttonIcon("zoom_in")));
-
   function truncate(text, length){
     let shortText = text.substring(0, length);
     if (text.length > length){
@@ -73,6 +90,95 @@ function createBoardElement(bob) {
     }
     return shortText;
   }
+
+  function addFullScreenModal($element, bob){
+    let modalLinkID = "link" + bob._id;
+    let animatedModalID = "modal" + bob._id;
+
+    let $zoomInButton = $('<a></a>')
+      .attr('href', "#" + animatedModalID)
+      .attr('id', modalLinkID)
+      .addClass("--nomargin")
+      .append($('<i></i>').addClass("material-icons tiny").append("zoom_in"));
+
+    let $modalScreen = $('<div></div').attr("id", animatedModalID);
+
+    let $closeDiv = $('<div></div')
+                      .addClass("close-" + animatedModalID)
+                      .attr("id", "btn-close-modal");
+    let $closeIcon = $("<i></i>")
+                    .addClass("waves-effect material-icons large")
+                    .append('close');
+    let $modalContentDiv = $('<div></div>').addClass("modal-content");
+
+    let $modalContent = function modalContent(){
+      switch (bob.flavor) {
+        case 'Quote':
+          $content = $('<div></div>').addClass("card white hoverable")
+            .append($('<div></div>').addClass("card-content black-text")
+              .append($('<p></p>').addClass("text--medium").append(truncate(bob.data.Text, 70)))
+              .append($('<span></span>').addClass("text--small right").append("by ", bob.data.Author)))
+          break;
+        case 'Text':
+          $content = $('<div></div>').addClass("card white hoverable")
+            .append($('<div></div>').addClass("card-content black-text")
+              .append(($('<p></p>')).addClass("flow-text-medium").append(truncate(bob.data.Text, 70))))
+          break;
+        case 'Poem':
+          $content = $('<div></div>').addClass("card white hoverable")
+            .append($('<div></div>').addClass("card-content black-text")
+              .append($('<span></span>').addClass("card-title").append(bob.data.Title))
+              .append($('<span></span>').addClass("right").append("by ", bob.data.Author)).append("<br>")
+              .append($('<p></p>').addClass("text--small").append(truncate(bob.data.Text, 200))))
+
+          break;
+        case 'Image':
+          $content = $('<div></div>').addClass("card white hoverable")
+            .append($('<div></div>').addClass("card-image")
+              .append($('<img></img>').addClass("responsive-img").attr('src', bob.data.Link)))
+
+          break;
+        case 'Meme':
+          $content = $('<div></div>').addClass("card white hoverable")
+            .append($('<span></span>').addClass("card-title").append(bob.data.Title))
+            .append($('<div></div>').addClass("card-image")
+              .append($('<img></img>').addClass("responsive-img").attr('src', bob.data.Link)))
+            .append($('<div></div>').addClass("card-content black-text")
+              .append(($('<p></p>')).addClass("flow-text-medium").append(truncate(bob.data.Description, 70))))
+
+          break;
+        case 'Video':
+          $content = $('<div></div>').addClass("card white hoverable")
+            .append($('<span></span>').addClass("card-title").append(bob.data.Title))
+            .append($('<div></div>').addClass("video-container")
+              .append($('<iframe></iframe>')
+                .attr("allowfullscreen","true").attr("frameborder", 0).attr('width', 853).attr('height',480).attr('src', bob.data.Link)))
+            .append($('<div></div>').addClass("card-content black-text")
+              .append(($('<p></p>')).addClass("flow-text-medium").append(truncate(bob.data.Description, 70))))
+          break;
+
+        default:
+          console.log("Unknown element type", bob.flavor);
+          $content= null;
+        }
+        return $content;
+    }
+
+    $modalScreen = $modalScreen
+      .append($closeDiv.append($closeIcon))
+      .append($modalContentDiv.append($modalContent));
+    $zoomInButton.appendTo($element);
+    $modalScreen.appendTo($element);
+  }
+
+
+  let $bobNav =
+  tagChips(bob)
+  .append($('<div></div>').addClass("card-action card-action--thin right-align")
+    .append(buttonIcon("thumb_up")));
+
+  addFullScreenModal($bobNav.find(".card-action"), bob)
+
   switch (bob.flavor) {
     case 'Quote':
       $html = $('<div></div>').addClass("card white hoverable")
@@ -127,7 +233,6 @@ function createBoardElement(bob) {
   }
   return $html;
 }
-
 
 var socket = io();
 socket.emit('connection');
