90a2ba3e4f17cd1439716a3c833de53790bfaa23
Adam Novotny <adam.novotny@postrock.wall>
2017-07-12 12:16:44 -0400

    Delete unused code

--- routes/sockets.js
+++ routes/sockets.js
@@ -2,13 +2,5 @@ module.exports = function(io, db) {
 
 	io.on('connection', function(socket) {
 		console.log("socket connected");
-
-		// db.Bob.getActiveBobs().then(function (bobs) {
-		// 	socket.emit('all_elements', bobs);
-		// });
-
-		// socket.on('manual_control', function(command){
-		// 	socket.broadcast.emit('manual_control', command);
-		// });
 	});
 };
