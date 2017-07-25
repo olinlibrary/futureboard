94286359775b6f08ea910f0c700d3ea718fbaf1c
Adam Novotny <adam.novotny@postrock.wall>
2017-07-25 12:00:42 -0400

    Move functions into module.exports so that they can access db and io

--- routes/awsapi.js
+++ routes/awsapi.js
@@ -35,103 +35,102 @@ module.exports = function (io, db) {
   router.route('/mediaStatus')
     .post(POSTSNSNotify);
 
-  return router;
-};
+  /*
+   * Respond to GET requests to /sign-s3.
+   * Upon request, return JSON containing the temporarily-signed S3 request and
+   * the anticipated URL of the image.
+   */
+  function GETSignRequest(req, res) {
+    const s3 = new aws.S3({
+        signatureVersion: 'v4',
+        // Endpoint can be switched to https://media.futureboard.olin.build/' after we get an ssl cert for media.FUTUREboard.olin.build
+        endpoint: new aws.Endpoint('https://s3.amazonaws.com')
+      });
+    const inputFileName = req.query['file-name'].split('.');
+    const fileType = req.query['file-type'];
+    const mediaType = fileType.split('/')[0]; // Get image or video
+    const inputFileExtension = inputFileName[inputFileName.length - 1];
+    let uploadFileName = '';
+    let outputFileName = '';
+    const new_uuid = uuidv1();
 
-
-/*
- * Respond to GET requests to /sign-s3.
- * Upon request, return JSON containing the temporarily-signed S3 request and
- * the anticipated URL of the image.
- */
-function GETSignRequest(req, res) {
-  const s3 = new aws.S3({
-      signatureVersion: 'v4',
-      // Endpoint can be switched to https://media.futureboard.olin.build/' after we get an ssl cert for media.FUTUREboard.olin.build
-      endpoint: new aws.Endpoint('https://s3.amazonaws.com')
-    });
-  const inputFileName = req.query['file-name'].split('.');
-  const fileType = req.query['file-type'];
-  const mediaType = fileType.split('/')[0]; // Get image or video
-  const inputFileExtension = inputFileName[inputFileName.length - 1];
-  let uploadFileName = '';
-  let outputFileName = '';
-  const new_uuid = uuidv1();
-
-  if(mediaType === 'image'){
-    uploadFileName = 'img-' + new_uuid + '.' + inputFileExtension;
-    outputFileName = 'img-' + new_uuid + '.jpg';
-  } else if (mediaType === 'video') {
-    uploadFileName = 'vid-' + new_uuid + '.' + inputFileExtension;
-    outputFileName = 'vid-' + new_uuid + '.mp4';
-  } else {
-    res.status(415).send('unsuported media type');
-    return;
-  }
-  console.log(mediaType, fileType, uploadFileName);
-  const s3Params = {
-    Bucket: S3_BUCKET,
-    Key: uploadFileName,
-    Expires: 60,
-    ContentType: fileType,
-    ACL: 'public-read'
-  };
-
-  s3.getSignedUrl('putObject', s3Params, function (err, data) {
-    if(err){
-      console.log(err);
-      return res.end();
+    if(mediaType === 'image'){
+      uploadFileName = 'img-' + new_uuid + '.' + inputFileExtension;
+      outputFileName = 'img-' + new_uuid + '.jpg';
+    } else if (mediaType === 'video') {
+      uploadFileName = 'vid-' + new_uuid + '.' + inputFileExtension;
+      outputFileName = 'vid-' + new_uuid + '.mp4';
+    } else {
+      res.status(415).send('unsuported media type');
+      return;
     }
-    const returnData = {
-      signedRequest: data,
-      // The location of the future media, to be used for previering and submitting a bob
-      url: 'http://media.futureboard.olin.build/' + outputFileName
+    console.log(mediaType, fileType, uploadFileName);
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: uploadFileName,
+      Expires: 60,
+      ContentType: fileType,
+      ACL: 'public-read'
     };
-    console.log(returnData);
-    res.write(JSON.stringify(returnData));
-    res.end();
-  });
-}
 
-function POSTS3Sign(req, res) {
-  res.send("success");
-}
-
-function POSTSNSNotify (req, res) {
-  // Recieve all data
-  var chunks = [];
-  req.on('data', function (chunk) {
-      chunks.push(chunk);
-  });
-  // After all data is received
-  req.on('end', function () {
-      try {
-        var message = JSON.parse(chunks.join(''));
-        // If it is a subscribtion confirmation, get the page
-        if(req.headers['x-amz-sns-message-type'] === 'SubscriptionConfirmation'){
-          request(message.SubscribeURL, function (err, res, body) {
-            if(err){ console.log(err); }
-          });
-        // Else set the bob media status to true
-        } else {
-          var SNSmessage = JSON.parse(message.Message);
-          SNSmessage.Records.forEach((record) => {
-            if(record.s3.object.key.indexOf('/') == -1){
-              db.Bob.setMediaStatus('http://media.futureboard.olin.build/' + record.s3.object.key, true)
-              .then(function (bobData) {
-                io.emit('add_element', bobData);
-              });
-
-              console.log("s3 ready:", record.s3.object.key);
-            }
-          });
-        }
-
-      } catch (e) {
-        // Errors caused by bad jso
-        console.log(e);
-        return;
+    s3.getSignedUrl('putObject', s3Params, function (err, data) {
+      if(err){
+        console.log(err);
+        return res.end();
       }
-  });
-  res.end();
-}
+      const returnData = {
+        signedRequest: data,
+        // The location of the future media, to be used for previering and submitting a bob
+        url: 'http://media.futureboard.olin.build/' + outputFileName
+      };
+      console.log(returnData);
+      res.write(JSON.stringify(returnData));
+      res.end();
+    });
+  }
+
+  function POSTS3Sign(req, res) {
+    res.send("success");
+  }
+
+  function POSTSNSNotify (req, res) {
+    // Recieve all data
+    var chunks = [];
+    req.on('data', function (chunk) {
+        chunks.push(chunk);
+    });
+    // After all data is received
+    req.on('end', function () {
+        try {
+          var message = JSON.parse(chunks.join(''));
+          // If it is a subscribtion confirmation, get the page
+          if(req.headers['x-amz-sns-message-type'] === 'SubscriptionConfirmation'){
+            request(message.SubscribeURL, function (err, res, body) {
+              if(err){ console.log(err); }
+            });
+          // Else set the bob media status to true
+          } else {
+            var SNSmessage = JSON.parse(message.Message);
+            SNSmessage.Records.forEach((record) => {
+              if(record.s3.object.key.indexOf('/') == -1){
+                db.Bob.setMediaStatus('http://media.futureboard.olin.build/' + record.s3.object.key, true)
+                .then(function (bobData) {
+                  io.emit('add_element', bobData);
+                });
+
+                console.log("s3 ready:", record.s3.object.key);
+              }
+            });
+          }
+
+        } catch (e) {
+          // Errors caused by bad jso
+          console.log(e);
+          return;
+        }
+    });
+    res.end();
+  }
+
+return router;
+};
