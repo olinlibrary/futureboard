27b25dfe9dfce98fe1fad153f8afc249bb09a997
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-07-25 12:21:48 -0400

    Add landing warning&welcom message

---
+++ static/js/landing.js
@@ -0,0 +1,5 @@
+$(function(){
+    Materialize.toast("Welcome to FutureBoard!", 2000);
+    Materialize.toast("NOTE: Lots of Media(Images, Videos)", 8000);
+    Materialize.toast("Watch out your Data!", 8000);
+});
